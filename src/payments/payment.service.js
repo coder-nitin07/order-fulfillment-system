ff87911b14ebdcb9303007b2e02ad5758b4ae349
coder-nitin07 <nitinthakur87504@gmail.com>
2025-12-23 18:28:41 +0530

    Setup payment functions

--- src/payments/payment.service.js
+++ src/payments/payment.service.js
@@ -1,4 +1,27 @@
 
-export const initiatePayment = async (authUserId, orderId, amount) => {};
+export const initiatePayment = async (authUserId, orderId, amount) => {
+    const result = await pool.query(
+        `
+            INSERT INTO payments (order_id, auth_user_id, amount, status)
+            VALUES ($1, $2, $3, 'INITIATED')
+            RETURNING *
+        `,
+        [ orderId, authUserId, amount ]
+    );
 
-export const getPaymentsByOrder = async (authUserId, orderId) => {};
+  return result.rows[0];
+};
+
+export const getPaymentsByOrder = async (authUserId, orderId) => {
+     const result = await pool.query(
+        `
+            SELECT *
+            FROM payments
+            WHERE order_id = $1 AND auth_user_id = $2
+            ORDER BY created_at DESC
+        `,
+        [ orderId, authUserId ]
+    );
+
+    return result.rows;
+};
