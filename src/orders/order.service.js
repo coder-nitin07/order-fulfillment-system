c9f3790a52e8516e9041d724b431b35745f71140
coder-nitin07 <nitinthakur87504@gmail.com>
2025-12-23 15:37:02 +0530

    create order service

--- src/orders/order.service.js
+++ src/orders/order.service.js
@@ -1,5 +1,19 @@
+import db from "../shared/db";
 
-export const createOrder = async (authUserId, orderData) => {};
+export const createOrder = async (authUserId, orderData) => {
+    const { total_amount } = orderData;
+
+    const result = await db.query(
+        `
+        INSERT INTO orders (auth_user_id, total_amount, status)
+        VALUES ($1, $2, 'CREATED')
+        RETURNING *
+        `,
+        [ authUserId, total_amount ]
+    );
+
+    return result.rows[0];
+};
 
 export const getMyOrders = async (authUserId) => {};
 
