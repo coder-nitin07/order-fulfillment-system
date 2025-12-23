382b74d1515cfdb29d1b07026e1ecdb6c7601e95
coder-nitin07 <nitinthakur87504@gmail.com>
2025-12-23 15:41:40 +0530

    create getOrder and byId service and update app file

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import errorHandler from './shared/error.handler.js';
 import authRouter from './auth/auth.routes.js';
 import userRouter from './users/user.routes.js';
+import orderRouter from './orders/order.routes.js';
 const app = express();
 
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(express.json());
 // routes
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 // error handler
 app.use(errorHandler);
--- src/orders/order.service.js
+++ src/orders/order.service.js
@@ -15,6 +15,30 @@ export const createOrder = async (authUserId, orderData) => {
     return result.rows[0];
 };
 
-export const getMyOrders = async (authUserId) => {};
+export const getMyOrders = async (authUserId) => {
+     const result = await pool.query(
+        `SELECT * FROM orders WHERE auth_user_id = $1 ORDER BY created_at DESC`,
+        [ authUserId ]
+    );
 
-export const getOrderById = async (orderId, authUserId) => {};
+    return result.rows;
+};
+
+export const getOrderById = async (orderId, authUserId) => {
+    const result = await pool.query(
+        `
+        SELECT *
+        FROM orders
+        WHERE id = $1 AND auth_user_id = $2
+        `,
+        [ orderId, authUserId ]
+    );
+
+    if (!result.rows.length) {
+        const err = new Error('Order not found');
+        err.statusCode = 404;
+        throw err;
+    }
+
+    return result.rows[0];
+};
