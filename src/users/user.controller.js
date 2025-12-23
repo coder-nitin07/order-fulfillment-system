f98f988c7ab64927f683063e4e4b84917f305745
coder-nitin07 <nitinthakur87504@gmail.com>
2025-12-23 14:25:19 +0530

    create user api

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -1,13 +1,37 @@
 import * as userService from './user.service.js';
 
 export const createProfileController = async (req, res, next) => {
-  res.send('Create user profile placeholder');
+  try {
+    const profile = await userService.createUserProfile(
+      req.user.userId,
+      req.body
+    );
+
+    res.status(201).json({ success: true, data: profile });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const getProfileController = async (req, res, next) => {
-  res.send('Get user profile placeholder');
+  try {
+    const profile = await userService.getUserProfile(req.user.userId);
+
+    res.json({ success: true, data: profile });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const updateProfileController = async (req, res, next) => {
-  res.send('Update user profile placeholder');
+  try {
+    const profile = await userService.updateUserProfile(
+      req.user.userId,
+      req.body
+    );
+
+    res.json({ success: true, data: profile });
+  } catch (err) {
+    next(err);
+  }
 };
